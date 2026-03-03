28191e21431a0ccd8ca7ce9bced13d272b9c6f31
Chris Troutner <chris.troutner@gmail.com>
2026-03-03 06:50:15 -0800

    Fixing nav menu

--- src/components/SiteNavbar.jsx
+++ src/components/SiteNavbar.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Container, Nav, Navbar } from 'react-bootstrap'
 import { NavLink } from 'react-router-dom'
 
@@ -11,17 +12,36 @@ const navItems = [
 ]
 
 function SiteNavbar() {
+  const [expanded, setExpanded] = useState(false)
+
+  const closeMenu = () => {
+    setExpanded(false)
+  }
+
   return (
-    <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
+    <Navbar
+      bg="dark"
+      variant="dark"
+      expand="lg"
+      sticky="top"
+      expanded={expanded}
+      onToggle={(nextExpanded) => setExpanded(nextExpanded)}
+    >
       <Container>
-        <Navbar.Brand as={NavLink} to="/">
+        <Navbar.Brand as={NavLink} to="/" onClick={closeMenu}>
           TheLocalTrade.app
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="main-nav" />
         <Navbar.Collapse id="main-nav">
           <Nav className="ms-auto">
             {navItems.map((item) => (
-              <Nav.Link as={NavLink} to={item.to} key={item.to} end={item.to === '/'}>
+              <Nav.Link
+                as={NavLink}
+                to={item.to}
+                key={item.to}
+                end={item.to === '/'}
+                onClick={closeMenu}
+              >
                 {item.label}
               </Nav.Link>
             ))}
